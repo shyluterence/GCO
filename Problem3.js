2566e511c61282a943fc779f950939013c885864
shylu thomas <shylu.thomas@version1.com>
2026-05-14 13:09:47 +0100

    - problem 3

---
+++ Problem3.js
@@ -0,0 +1,49 @@
+
+
+/*(0,0)→(0,1)→(1,1)→(1,2)→(2,2)
+(0,4)
+(2,4)→(3,4)→(3,3)
+(3,0)
+*/
+const grid = [
+  [1, 1, 0, 0, 1],
+  [0, 1, 1, 0, 0],
+  [0, 0, 1, 0, 1],
+  [1, 0, 0, 1, 1],
+  [0, 0, 1, 0, 0]
+];
+
+const visited = [];
+for (let i = 0; i < 5; i++) {
+  visited.push([false, false, false, false, false]);
+}
+
+function explore(row, col) {
+  if (row < 0 || row >= 5) return;
+  if (col < 0 || col >= 5) return;
+  if (visited[row][col]) return;
+  if (grid[row][col] === 0) return;
+
+  visited[row][col] = true;
+
+  explore(row - 1, col); // up
+  explore(row + 1, col); // down
+  explore(row, col - 1); // left
+  explore(row, col + 1); // right
+}
+
+let clusters = 0;
+
+for (let row = 0; row < 5; row++) {
+  for (let col = 0; col < 5; col++) {
+    if (grid[row][col] === 1 && !visited[row][col]) {
+      explore(row, col);
+      clusters++;
+    }
+  }
+}
+
+
+console.log(clusters); 
+
+//Answer :5
